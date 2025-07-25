8d6c736c024b1d09bada30c94be7eb69536c745f
capellaapparel <capella4fb@gmail.com>
2025-07-25 14:00:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,8 +137,8 @@ if page == "📖 스타일 정보 조회":
                 latest_temu = get_latest_temu_price(df_temu, selected)
                 if latest_shein is not None and str(latest_shein).strip() != "":
                     st.markdown(f"**SHEIN PRICE:** ${latest_shein}")
-                if latest_temu is not None and str(latest_temu).strip() != "":
-                    st.markdown(f"**TEMU PRICE:** ${latest_temu}")
+                if latest_temu = get_latest_temu_price(df_temu, selected)
+                    st.markdown(f"**TEMU PRICE:** {latest_temu}")
 
                 # 빈 정보 자동 생략
                 for col, label in [

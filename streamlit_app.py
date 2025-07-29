65f6440474b9938eb4c952a7c889406eb52f5a21
capellaapparel <capella4fb@gmail.com>
2025-07-29 10:51:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+from style_info_page import style_info_page
+from sales_dashboard_page import sales_dashboard_page
 
-st.set_page_config(page_title="Capella Product Dashboard", layout="wide")
-st.sidebar.title("Capella Dashboard")
-st.write("사이드 메뉴에서 페이지를 선택하세요.")
+page = st.sidebar.radio("페이지 선택", ["📖 스타일 정보 조회", "📊 세일즈 대시보드"])
+
+if page == "📖 스타일 정보 조회":
+    style_info_page()
+elif page == "📊 세일즈 대시보드":
+    sales_dashboard_page()
